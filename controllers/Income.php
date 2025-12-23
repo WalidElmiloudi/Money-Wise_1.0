514b9eb4a740554f3523125ddfa3d5a19154048f
WalidElmiloudi <elwalid38@gmail.com>
2025-12-23 19:21:42 +0100

    completing the income class with all his necessary attributes and methodes

---
+++ controllers/Income.php
@@ -0,0 +1,56 @@
+<?php
+
+require 'Database.php';
+
+class Income{
+  private $user_id;
+
+  function __construct($user_id){
+    $this->user_id = $user_id;
+  }
+
+  public function add($amount,$category,$description){
+    $conn = new Database('localhost','money_wallet','root','');
+    $pdo = $conn->connect();
+    $stmt = $pdo->prepare("INSERT INTO incomes (montant,description,category,userID) VALUES (:montant,:description,:category,:userID)");
+    $stmt->execute([
+      ':montant'=>$amount,
+      ':description'=>$description,
+      ':category'=>$category,
+      ':userID'=>$this->user_id
+    ]);
+  }
+
+  public function update($amount,$category,$description,$income_id){
+    $conn = new Database('localhost','money_wallet','root','');
+    $pdo = $conn->connect();
+    $stmt = $pdo->prepare("UPDATE incomes SET montant = :montant,description = :description,category = :category WHERE id = :id");
+    $stmt->execute([
+      ':montant'=>$amount,
+      ':description'=>$description,
+      ':category'=>$category,
+      ':id'=>$income_id
+    ]);
+  }
+
+  public function delete($income_id){
+    $conn = new Database('localhost','money_wallet','root','');
+    $pdo = $conn->connect();
+    $stmt = $pdo->prepare("DELETE FROM incomes WHERE id = :id");
+    $stmt->execute([
+      ':id'=>$income_id
+    ]);
+    }
+
+  public function getAll(){
+    $conn = new Database('localhost','money_wallet','root','');
+    $pdo = $conn->connect();
+    $stmt = $pdo->prepare("SELECT * FROM incomes WHERE userID = :user_id");
+    $stmt->execute([
+      ':user_id'=>$this->user_id
+    ]);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $results;
+  }
+}
+
